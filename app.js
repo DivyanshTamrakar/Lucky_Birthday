679d9617e6f08c4b50ba123aa6a1eb03165a3b62
DivyanshTamrakar <tamrakardivyansh189@gmail.com>
2021-01-09 01:47:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+//Declaration
+
+var birthinput = document.querySelector(".birthday");
+var submit = document.querySelector(".submit-type");
+var num = document.querySelector(".lucky-no");
+var dial = document.querySelector(".dialog");
+var out = document.querySelector(".modal");
+var txt = document.querySelector(".para");
+var img = document.querySelector(".image");
+var content = document.querySelector(".modal-content");
+content.style.display = "none";
+out.style.display = "none";
+submit.addEventListener("click", getResult);
+
+//Functions
+function getResult(e) {
+    e.preventDefault();
+
+
+    if (birthinput.value === "") {
+        alert("Please Enter Birthday");
+    } else {
+
+        if (num.value === "") {
+            alert("Please Enter Your Lucky Number ");
+
+        } else {
+            var str = birthinput.value;
+
+            var split = str.split("-").join("");
+            var mod = 0;
+            var length = split.length;
+            for (var i = 0; i < length; i++) {
+                mod = mod + split % 10;
+                split = parseInt(split / 10);
+            }
+
+            if (mod % num.value === 0) {
+                content.style.display = "block";
+                out.style.display = "block";
+                txt.innerText = "!!Hurray your birthday is Lucky!!";
+
+            } else {
+
+                out.style.display = "block";
+                content.style.display = "block";
+
+                txt.innerText = "!! Oops your birthday is not Lucky!!";
+            }
+
+
+
+
+        }
+    }
+
+
+}
